41dc6cda982a67cd7965b1a3dacef64636957e7d
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2022-02-02 13:59:20 +0700

    First simple console app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-console.log(number);
-var number = 15;
+let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+let personalMovieDB = {
+    count: [],
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false,
+}
+
+let movie1 = prompt('Один из последних просмотренных фильмов?'),
+    rate1 = prompt('На сколько оцените его?'),
+    movie2 = prompt('Один из последних просмотренных фильмов?'),
+    rate2 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[movie1] = rate1;
+personalMovieDB.movies[movie2] = rate2;
+
+console.log(personalMovieDB)
